9d42004bf965fdadd0f638850cfd3b08d59254b8
Alan Oliveira <alancjobm@gmail.com>
2021-04-18 00:12:27 -0300

    criando a primeira route, controller e view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # definindo home page
+  root "articles#index"
+
+  # configurando minha primeira rota
+  get "/articles", to: "articles#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
