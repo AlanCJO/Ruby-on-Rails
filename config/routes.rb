6d34f0b8d1b83731df12013e5f5f844942421a2e
Alan Oliveira <alancjobm@gmail.com>
2021-04-18 11:00:42 -0300

    simplificando com resource e link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root "articles#index"  # definindo home page
 
+=begin
   get "/articles", to: "articles#index"  # configurando minha primeira rota
   get "/articles/:id", to: "articles#show" # mostrando os artigos
+=end
+  # entidades com operações CRUD são chamadas de recursos
+  resources :articles # cria automaticamente as operações de CRUD
+
+
 
 end
